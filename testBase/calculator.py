6fcfe0b588fa5bc603ae805fca2fbdbe71a1d082
doaitran <doai.tran@titancorpvn.com>
2019-03-24 20:37:19 +0700

    update calculator

--- testBase/calculator.py
+++ testBase/calculator.py
@@ -8,12 +8,15 @@ class calculator:
 
     def get_first(self):
         self.num_1 = float(input("Input the first number: "))
+        return self.num_1
 
     def get_second(self):
         self.num_2 = float(input("Input the second number: "))
+        return self.num_2
 
     def get_operator(self):
         self.operator = float(input("Input the operation: "))
+        return self.operator
 
     def calculation(self):
         pass
