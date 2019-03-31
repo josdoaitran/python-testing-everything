7d71a4edcbcfc34fcf4c4ceaec638e1e8a9dd5a3
doaitran <doai.tran@titancorpvn.com>
2019-03-31 18:11:53 +0700

    update

--- testBase/calculator.py
+++ testBase/calculator.py
@@ -1,29 +1,30 @@
+## Base on Python 3
 
-class calculator:
-    def __init__(self):
+class Calculator:
+    def __init__(self):
         self.num_1 = None
         self.num_2 = None
         self.operator = None
         self.result = None
 
-    def get_first(self):
+    def get_first(self):
         self.num_1 = float(input("Input the first number: "))
         return self.num_1
 
-    def get_second(self):
+    def get_second(self):
         self.num_2 = float(input("Input the second number: "))
         return self.num_2
 
     def get_operator(self):
-        self.operator = float(input("Input the operation: "))
+        self.operator = (input("operator: "))
         return self.operator
 
     def calculation(self):
         pass
 
 
-calculation = Calculation()
+calculator = Calculator()
 
-calculation.get_first()
-calculation.get_second()
-calculation.get_operator()
+calculator.get_first()
+calculator.get_second()
+calculator.get_operator()
