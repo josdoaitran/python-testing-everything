c024973996e7465a73662cec90bf41f7f9940cf3
josdoaitran <josdoaitran@gmail.com>
2019-04-10 11:50:56 +0700

    update Q2

--- BasicQuestions/Q2.py
+++ BasicQuestions/Q2.py
@@ -1,8 +1,13 @@
+from pip._vendor.distlib.compat import raw_input
+
+
 def fact(x):
     if x == 0:
         return 1
     return x * fact(x - 1)
 
 x=int(raw_input())
-print fact(x)
+print (fact(x))
+
 #----------------------------------------#
+
