c48318d869d8ed62ca6a1f4acd947ee12797e5e9
josdoaitran <josdoaitran@gmail.com>
2019-04-15 09:01:45 +0700

    update simple fucntion

--- testBase/tutorial_simple_function.py
+++ testBase/tutorial_simple_function.py
@@ -1,9 +1,9 @@
 # Simple function in Python
 
-def helloWorl()
-   print(“Hello World”)
+def helloWorld():
+   print("Hello World")
 
 
-print (“The first function”)
+print ("The first function")
 helloWorld()
-print (“Have a nice day”)
+print ("Have a nice day")
