5c215c7c113c5d66be14a78369b4720404c91b3f
josdoaitran <josdoaitran@gmail.com>
2019-04-15 12:43:39 +0700

    update tutorial3

---
+++ testBase/tutorial3_String.py
@@ -0,0 +1,8 @@
+# We have the string as
+first_string = "Hello! We are funny coders"
+
+# To print the characters from begin to 9th character
+print (first_string[0:9])
+
+# To print all characters
+print (first_string[0:])
