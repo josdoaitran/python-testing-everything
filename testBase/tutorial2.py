0f81aa6189ab58a217a895aecc5c945a66aaa9a2
josdoaitran <josdoaitran@gmail.com>
2019-04-15 17:25:39 +0700

    update date

--- testBase/tutorial2.py
+++ testBase/tutorial2.py
@@ -7,3 +7,9 @@ birth_year = input("Please input your birth year: ")
 age = datetime.datetime.now().year - int(birth_year)
 
 print("Your age: " + str(age))
+
+###################
+
+import datetime
+now = datetime.datetime.now()
+print (now.year, now.month, now.day, now.hour, now.minute, now.second)
