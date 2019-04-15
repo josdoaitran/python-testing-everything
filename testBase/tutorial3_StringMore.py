ac17fc9c36294b3b5080749cb58fe33cc7d68163
josdoaitran <josdoaitran@gmail.com>
2019-04-15 17:48:25 +0700

    update string to list

--- testBase/tutorial3_StringMore.py
+++ testBase/tutorial3_StringMore.py
@@ -3,3 +3,14 @@
 init_string = "We are learning Python coding programming"
 length_str = len(init_string)
 print ("The length of String: " + str(length_str))
+
+# Convert String to list
+init_string = init_string.split(" ")
+print("After: ")
+print(init_string)
+
+text = 'a,b,c'
+print("Before: "+text)
+text = text.split(',')
+print("After: ")
+print(text)
