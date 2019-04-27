baed72d4bbcfca3f7d67850d9e04b42b4aa4e47a
josdoaitran <josdoaitran@gmail.com>
2019-04-27 20:59:14 +0700

    update list statement

--- testBase/tutorial4-list.py
+++ testBase/tutorial4-list.py
@@ -7,3 +7,11 @@ print(programming_language[1])
 print(programming_language[1:])
 # print element 2nd to 4th of list
 print(programming_language[2:4])
+
+####################
+lucky_number = ['45', '7', '19', '1', '88', '9999', '14']
+friend_list = ['Tom', 'Duck', 'John', 'Smith', 'Guru']
+
+# Combine lists
+friend_list.extend(lucky_number)
+print(friend_list)
