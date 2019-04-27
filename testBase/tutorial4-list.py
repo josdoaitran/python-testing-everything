523dbb1f6e68d78e4e0924fcf22d91ad230fed64
josdoaitran <josdoaitran@gmail.com>
2019-04-27 20:33:39 +0700

    update test for List

---
+++ testBase/tutorial4-list.py
@@ -0,0 +1,9 @@
+programming_language = ['Java', 'C++', 'C#', 'Python', 'Ruby', 'javascript']
+# print full elemnents of list
+print(programming_language)
+# print element 1 of list
+print(programming_language[1])
+# print element 2 to end of list
+print(programming_language[1:])
+# print element 2nd to 4th of list
+print(programming_language[2:4])
