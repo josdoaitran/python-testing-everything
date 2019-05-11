8cc9078d195bf8288de1629642205bd883c99838
josdoaitran <josdoaitran@gmail.com>
2019-05-11 21:03:32 +0700

    update lôpin

---
+++ testBase/tutorial5_Looping_Iterator.py
@@ -0,0 +1,7 @@
+# While Looping: while the statement is True, the code inside the block will be executed.
+# So, this code will print the number from 1 to 10
+num = 1
+
+while num <= 10:
+    print(num)
+    num += 1
