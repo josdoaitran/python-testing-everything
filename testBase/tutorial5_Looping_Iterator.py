ee87a59b7d9fadef174a6e789128326a2ebb0ef2
josdoaitran <josdoaitran@gmail.com>
2019-05-11 21:07:36 +0700

    update lôpin

--- testBase/tutorial5_Looping_Iterator.py
+++ testBase/tutorial5_Looping_Iterator.py
@@ -5,3 +5,9 @@ num = 1
 while num <= 10:
     print(num)
     num += 1
+
+loop_condition = True
+
+while loop_condition:
+    print("Loop Condition keeps: %s" %(loop_condition))
+    loop_condition = False
