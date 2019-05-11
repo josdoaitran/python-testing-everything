819cce6395b686e6350afdb72ab0afa210fdab3a
josdoaitran <josdoaitran@gmail.com>
2019-05-11 21:12:50 +0700

    update lôpin

---
+++ testBase/Key-Value Data Structure/LoopingThroughDataStructures.py
@@ -0,0 +1,16 @@
+bookshelf = [
+  "The Effective Engineer",
+  "The 4 hours work week",
+  "Zero to One",
+  "Lean Startup",
+  "Hooked"
+]
+
+for book in bookshelf:
+    print(book)
+
+# The Effective Engineer
+# The 4 hours work week
+# Zero to One
+# Lean Startup
+# Hooked
