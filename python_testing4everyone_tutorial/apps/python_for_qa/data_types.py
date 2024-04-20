4001a69113ab8c7d108453208bc159685ecb911f
Doai Tran <josdoaitran@gmail.com>
2024-04-20 22:41:03 +0700

    update example code

---
+++ python_testing4everyone_tutorial/apps/python_for_qa/data_types.py
@@ -0,0 +1,7 @@
+age: int = 22
+students_list: list ['Tom', 'Jack', 'Anna']
+data: dict[str, str] = {"automation-test": "ID-3456", "status": "Passed"}
+
+
+def double_age(age: int):
+    return age*2
