ef72490c13afc6b666e56d04a9650f9f815d57c2
Doai Tran <josdoaitran@gmail.com>
2024-04-20 22:44:14 +0700

    update example code

--- python_testing4everyone_tutorial/apps/python_for_qa/data_types.py
+++ python_testing4everyone_tutorial/apps/python_for_qa/data_types.py
@@ -3,5 +3,3 @@ students_list: list ['Tom', 'Jack', 'Anna']
 data: dict[str, str] = {"automation-test": "ID-3456", "status": "Passed"}
 
 
-def double_age(age: int):
-    return age*2
---
+++ python_testing4everyone_tutorial/apps/python_for_qa/methods_data_types.py
@@ -0,0 +1,5 @@
+
+def calculate_compound_knowledge(no_years: int, interest: float) -> float:
+    return no_years*interest
+
+
