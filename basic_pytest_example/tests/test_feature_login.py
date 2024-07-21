befa898fb4177e889daef10833e210d9cff12838
Doai Tran <josdoaitran@gmail.com>
2024-07-21 14:28:35 +0700

    init example code for tutorials and testing4everyone

---
+++ basic_pytest_example/tests/test_feature_login.py
@@ -0,0 +1,7 @@
+def test_feature_login_happy_case():
+    print('test_feature_login')
+    assert True
+
+def test_feature_login_unhappy_case():
+    print('test_feature_login_unhappy_case')
+    assert False
---
+++ basic_pytest_example/tests/test_feature_payment.py
@@ -0,0 +1,7 @@
+def test_feature_payment_happy_case():
+    print('test_feature_pament')
+    assert True
+
+def test_feature_payment_unhappy_case():
+    print('test_feature_payment_unhappy_case')
+    assert False
