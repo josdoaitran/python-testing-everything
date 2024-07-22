f50865d3237d77d48bad9d2259060ccf75a84318
Doai Tran <josdoaitran@gmail.com>
2024-07-22 15:11:07 +0700

    init code

---
+++ basic_pytest_example/tests/wrong_folder/basic.py
@@ -0,0 +1,12 @@
+def test_function():
+    print("test_function")
+
+
+def test_function_2():
+    print("test_function")
+
+def test_function_3():
+    print("test_function")
+
+def test_function_4():
+    print("test_function")
